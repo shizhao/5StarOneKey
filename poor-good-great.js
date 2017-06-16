b62ffa90003223a958ca41f98fd5636659aa2736
shizhao <shizhao@gmail.com>
2017-06-16 12:02:56 +0800

    Update poor-good-great.js

--- poor-good-great.js
+++ poor-good-great.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         5 Star One Key
+// @name         poor, good, great One Key
 // @version      0.1
-// @description  Give five star with single click
-// @updateURL    https://github.com/jqqqqqqqqqq/5StarOneKey/raw/master/5%20Star%20One%20Key.user.js
-// @downloadURL  https://github.com/jqqqqqqqqqq/5StarOneKey/raw/master/5%20Star%20One%20Key.user.js
-// @author       jqqqqqqqqqq
+// @description  Give 1-5 star with single click. fork https://github.com/jqqqqqqqqqq/5StarOneKey
+// @updateURL    https://github.com/shizhao/5StarOneKey/raw/master/poor-good-great.js
+// @downloadURL  https://github.com/shizhao/5StarOneKey/raw/master/poor-good-great.js
+// @author       jqqqqqqqqqq, shizhao
 // @match        https://opr.ingress.com/recon
 // @grant        none
 // ==/UserScript==
@@ -15,7 +15,7 @@
     var target1 = document.getElementById("submitDiv");
     
     var five_star = document.createElement("button");
-    var textnode = document.createTextNode("Five Star");
+    var textnode = document.createTextNode("穷");
     five_star.className = "button big-submit-button";
     five_star.appendChild(textnode);
     target1.appendChild(five_star);
@@ -29,7 +29,7 @@
     };
     
     var _533355 = document.createElement("button");
-    var textnode = document.createTextNode("533355");
+    var textnode = document.createTextNode("好");
     _533355.className = "button big-submit-button";
     _533355.appendChild(textnode);
     target1.appendChild(_533355);
@@ -43,16 +43,16 @@
     };
     
     var _553355 = document.createElement("button");
-    var textnode = document.createTextNode("553355");
+    var textnode = document.createTextNode("大");
     _553355.className = "button big-submit-button";
     _553355.appendChild(textnode);
     target1.appendChild(_553355);
     _553355.onclick = function(){
-        document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > div.btn-group > button:nth-child(5)").click();
-        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(5) > button:nth-child(5)").click();
-        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(10) > button:nth-child(3)").click();
-        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(15) > button:nth-child(3)").click();
-        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > button:nth-child(5)").click();
-        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(10) > button:nth-child(5)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > div.btn-group > button:nth-child(1)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(5) > button:nth-child(1)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(10) > button:nth-child(1)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(15) > button:nth-child(1)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(5) > button:nth-child(1)").click();
+        document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(10) > button:nth-child(1)").click();
     };
 })();
