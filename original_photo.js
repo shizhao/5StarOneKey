5b4728187326100f4599f63bbc4c8ca53a8f2add
shizhao <shizhao@gmail.com>
2017-07-03 20:46:27 +0800

    Create original_photo.js

---
+++ original_photo.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         show original photo
+// @version      0.1
+// @description  点击图片，显示原始照片，配合Exif viewer使用: https://chrome.google.com/webstore/detail/exif-viewer/nafpfdcmppffipmhcpkbplhkoiekndck
+// @source      
+// @updateURL   
+// @downloadURL  
+// @author       shizhao
+// @match        https://opr.ingress.com/recon
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementsByClassName("ingress-background")[0].onclick=function(){
+        var orignsrc = document.getElementsByClassName("center-cropped-img")[0].getAttribute("ng-src");
+        var modalbody = document.getElementsByClassName("modal-body");
+        var img =modalbody[0].getElementsByTagName("img")[0];
+        img.src=orignsrc+"=s0";
+       console.log(orignsrc);  
+        console.log(img.src);
+   };
+    // Your code here...
+})();
