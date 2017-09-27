2ab5a40e777e9f41bd7e4a2d42235d06b9e174e3
shizhao <shizhao@gmail.com>
2017-09-27 22:18:23 +0800

    Update OPRrefresh.user.js

--- OPRrefresh.user.js
+++ OPRrefresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OPR刷新
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  自动刷新，直到有新po出现
 // @author       shizhao
 // @source       https://github.com/shizhao/5StarOneKey
@@ -10,22 +10,24 @@
 // @match        https://opr.ingress.com/*
 // @grant        none
 // ==/UserScript==
+console.log(document.readyState);
 async function opr() {
-    if(document.querySelector("div.row > div.alert-danger.ng-hide > p.ng-binding") && location.href == "https://opr.ingress.com/recon"){
-        await sleep(5000);
-       if(document.querySelector("div.row > div.alert-danger.ng-hide > p.ng-binding").innerText.replace(/\s+/g,"").length !== 0){
+    if(document.querySelector("div.row > div.alert-danger > p.ng-binding") && location.href == "https://opr.ingress.com/recon"){
+        await sleep(10000);
+       if(document.querySelector("div.row > div.alert-danger > p.ng-binding").innerText.replace(/\s+/g,"").length !== 0){
         rd = rnd(1,10);
         console.log("/",rd);
         await sleep(rd);
         location.href="/";
         }
-    }
-    if(document.querySelector("p > a > button.button")){
+    }else if(document.querySelector("p > a > button.button")){
         rd=rnd(10,30);
         console.log("/recon",rd);
         await sleep(rd);
         location.href="/recon";
     }
+}else{
+    document.title = '(新！)'+document.title;
 }
 
 function sleep(ms) {
@@ -35,4 +37,5 @@ function sleep(ms) {
 function rnd(start, end){
     return Math.floor((Math.random() * (end - start) + start) * 1000);
 }
+console.log(document.readyState);
 onload=opr();
