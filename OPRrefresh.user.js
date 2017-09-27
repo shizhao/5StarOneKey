c40039c511985fc205d386c50eea48782edb8579
shizhao <shizhao@gmail.com>
2017-09-27 22:22:39 +0800

    Update OPRrefresh.user.js

--- OPRrefresh.user.js
+++ OPRrefresh.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         OPR刷新
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  自动刷新，直到有新po出现
 // @author       shizhao
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @source       https://github.com/shizhao/5StarOneKey
 // @updateURL    https://github.com/shizhao/5StarOneKey/raw/master/OPRrefresh.user.js
 // @downloadURL  https://github.com/shizhao/5StarOneKey/raw/master/OPRrefresh.user.js
 // @match        https://opr.ingress.com/*
 // @grant        none
 // ==/UserScript==
-console.log(document.readyState);
+
 async function opr() {
     if(document.querySelector("div.row > div.alert-danger > p.ng-binding") && location.href == "https://opr.ingress.com/recon"){
         await sleep(10000);
@@ -25,9 +26,9 @@ async function opr() {
         console.log("/recon",rd);
         await sleep(rd);
         location.href="/recon";
-    }
-}else{
+    }else{
     document.title = '(新！)'+document.title;
+}
 }
 
 function sleep(ms) {
@@ -37,5 +38,5 @@ function sleep(ms) {
 function rnd(start, end){
     return Math.floor((Math.random() * (end - start) + start) * 1000);
 }
-console.log(document.readyState);
+
 onload=opr();
