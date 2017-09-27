905a854ab8c75a69812ae5f5a2116be41c706e26
shizhao <shizhao@gmail.com>
2017-09-27 22:41:45 +0800

    Update OPRrefresh.user.js

--- OPRrefresh.user.js
+++ OPRrefresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OPR刷新
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  自动刷新，直到有新po出现
 // @author       shizhao
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
@@ -14,12 +14,14 @@
 
 async function opr() {
     if(document.querySelector("div.row > div.alert-danger > p.ng-binding") && location.href == "https://opr.ingress.com/recon"){
-        await sleep(10000);
-       if(document.querySelector("div.row > div.alert-danger > p.ng-binding").innerText.replace(/\s+/g,"").length !== 0){
         rd = rnd(1,10);
-        console.log("/",rd);
-        await sleep(rd);
-        location.href="/";
+        console.log("/",10000+rd);
+        await sleep(10000+rd);
+       if(document.querySelector("div.row > div.alert-danger > p.ng-binding").innerText.replace(/\s+/g,"").length !== 0){
+        //rd = rnd(1,10);
+        //console.log("/",rd);
+        //await sleep(rd);
+            location.href="/";
         }
     }
     if(document.querySelector("p > a > button.button")){
